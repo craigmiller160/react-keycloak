18fb67afba9167bded2991e6adb582bb2f4e91d9
Craig Miller <craigmiller160@gmail.com>
2023-01-29 12:20:41 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	extends: [
+		'@craigmiller160/eslint-config-js',
+		'@craigmiller160/eslint-config-prettier',
+		'@craigmiller160/eslint-config-react',
+		'@craigmiller160/eslint-config-tree-shaking-import-restrictions',
+		'@craigmiller160/eslint-config-ts'
+	]
+};
