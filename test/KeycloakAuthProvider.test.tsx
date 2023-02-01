9583d47e91610499681806c86b4cac6ff308a427
Craig Miller <craigmiller160@gmail.com>
2023-02-01 17:22:19 -0500

    Stubbed out tests

---
+++ test/KeycloakAuthProvider.test.tsx
@@ -0,0 +1,23 @@
+import { describe, it } from 'vitest';
+
+describe('KeycloakAuthProvider', () => {
+	it('initializes authentication on render', () => {
+		throw new Error();
+	});
+
+	it('handles a successful authentication', () => {
+		throw new Error();
+	});
+
+	it('handles a failed authentication', () => {
+		throw new Error();
+	});
+
+	it('handles a successful authentication with the required roles', () => {
+		throw new Error();
+	});
+
+	it('handles a successful authentication without the required roles', () => {
+		throw new Error();
+	});
+});
--- test/hello.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('Hello World test', () => {
-	it('say hello world', () => {
-		const message = 'Hello World';
-		expect(message).toEqual('Hello World');
-	});
-});
