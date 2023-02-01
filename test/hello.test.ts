7b678d6a0cc73b4446441796ccbc9f5a52915143
Craig Miller <craigmiller160@gmail.com>
2023-02-01 17:18:12 -0500

    Fixed test

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 
 describe('Hello World test', () => {
 	it('say hello world', () => {
-		const message = 'Hello world';
+		const message = 'Hello World';
 		expect(message).toEqual('Hello World');
 	});
 });
