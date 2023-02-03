9a24fc73bdcf75992d023992ddac5c691d4ed6ae
Craig Miller <craigmiller160@gmail.com>
2023-02-03 16:03:20 -0500

    Feature/inf2 93 (#1)
    * Adding new types and props
    
    * Added a new state for the auth status
    
    * Setting up vitest
    
    * Tried running tests
    
    * Fixed test
    
    * Got vitest working
    
    * Setup vitest for react stuff
    
    * Stubbed out tests
    
    * Exported a type
    
    * Basic setup
    
    * Beginning work on mocking keycloak and clearing refresh interval
    
    * More work on keycloak mock
    
    * Trying to get test to run
    
    * Getting closer to having test work
    
    * Updated auth provider
    
    * Making progress with tests
    
    * More progress with test setup
    
    * More progress with tests
    
    * Got tests to pass
    
    * Removed unnecessary test stub
    
    * Updated mocks and tests
    
    * Updated tests
    
    * Got another test working
    
    * Stubbed out another test
    
    * Upgrading mock token
    
    * More improvements to test setup
    
    * More work on test setup
    
    * More test setup
    
    * Setup common test logic
    
    * Cleanup
    
    * Updated test logic
    
    * Implemented remaining test logic

---
+++ test/mocks/MockKeycloak.ts
@@ -0,0 +1,61 @@
+import type {
+	KeycloakConfig,
+	KeycloakInitOptions,
+	KeycloakTokenParsed
+} from 'keycloak-js';
+
+export const DEFAULT_TOKEN = 'ABCDEFG';
+
+const DEFAULT_TOKEN_PARSED: KeycloakTokenParsed = {
+	sub: 'mock-token'
+};
+
+export class MockKeycloak {
+	static lastConfig?: KeycloakConfig = undefined;
+	static lastInit?: KeycloakInitOptions = undefined;
+	private static authShouldSucceed = false;
+	private static tokenParsed?: KeycloakTokenParsed;
+
+	static setAuthResult(
+		authShouldSucceed: boolean,
+		tokenParsed?: KeycloakTokenParsed
+	) {
+		if (authShouldSucceed && !tokenParsed) {
+			throw new Error(
+				'Must specify the tokenParsed argument if authorization should succeed'
+			);
+		}
+		MockKeycloak.authShouldSucceed = authShouldSucceed;
+		MockKeycloak.tokenParsed = tokenParsed;
+	}
+
+	static reset() {
+		MockKeycloak.lastConfig = undefined;
+		MockKeycloak.lastInit = undefined;
+		MockKeycloak.authShouldSucceed = false;
+		MockKeycloak.tokenParsed = undefined;
+	}
+
+	token?: string;
+	tokenParsed?: KeycloakTokenParsed;
+	constructor(config: KeycloakConfig) {
+		MockKeycloak.lastConfig = config;
+	}
+
+	init(options: KeycloakInitOptions): Promise<boolean> {
+		MockKeycloak.lastInit = options;
+		if (MockKeycloak.authShouldSucceed) {
+			this.token = DEFAULT_TOKEN;
+			this.tokenParsed = DEFAULT_TOKEN_PARSED;
+		}
+
+		return new Promise((resolve) =>
+			resolve(MockKeycloak.authShouldSucceed)
+		);
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	updateToken(minValidity: number): Promise<boolean> {
+		return new Promise((resolve) => resolve(true));
+	}
+}
---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+	plugins: [react()],
+	test: {
+		watch: false,
+		environment: 'jsdom',
+		setupFiles: './test/setup.ts'
+	}
+});
