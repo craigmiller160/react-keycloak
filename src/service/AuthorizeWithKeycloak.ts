6458562b031144fb9e2102189b7d664e08caa280
Craig Miller <craigmiller160@gmail.com>
2023-02-03 16:11:04 -0500

    More progress building methods

--- src/service/AuthorizeWithKeycloak.ts
+++ src/service/AuthorizeWithKeycloak.ts
@@ -1,3 +1,28 @@
 import { KeycloakAuthConfig } from './KeycloakAuthConfig';
+import Keycloak from 'keycloak-js';
+import { KeycloakAuth } from '../KeycloakAuth';
 
-export const authorizeWithKeycloak = (config: KeycloakAuthConfig) => {};
+type AuthState = 'pre-auth' | 'authorizing' | 'role-check' | 'post-auth';
+
+type AuthContext = {
+	readonly state: AuthState;
+	readonly config: KeycloakAuthConfig;
+	readonly keycloak: Keycloak;
+};
+
+const handleAuthStep = (context: AuthContext): Promise<unknown> => {};
+
+export const authorizeWithKeycloak = (
+	config: KeycloakAuthConfig
+): Promise<KeycloakAuth> => {
+	const keycloak = new Keycloak({
+		url: config.authServerUrl,
+		realm: config.realm,
+		clientId: config.clientId
+	});
+	return handleAuthStep({
+		state: 'pre-auth',
+		config,
+		keycloak
+	});
+};
