0dc0482d0f2dd30e015cda360ee3ceaffad5c6de
Craig Miller <craigmiller160@gmail.com>
2023-02-03 16:05:10 -0500

    Setup new core types

---
+++ src/KeycloakAuth.ts
@@ -0,0 +1,11 @@
+import { KeycloakTokenParsed } from 'keycloak-js';
+
+export type KeycloakAuthStatus = 'authorized' | 'unauthorized';
+
+export type LogoutFn = () => void;
+
+export type KeycloakAuth = {
+	readonly authStatus: KeycloakAuthStatus;
+	readonly logout: LogoutFn;
+	readonly token?: KeycloakTokenParsed;
+};
