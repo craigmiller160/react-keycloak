04603ba9ded31c7235c757a6641db8eba7f833aa
Craig Miller <craigmiller160@gmail.com>
2023-02-04 14:27:49 -0500

    Beginning work on authorization logic

--- src/core/authorizeWithKeycloak.ts
+++ src/core/authorizeWithKeycloak.ts
@@ -1,5 +1,47 @@
 import { AuthorizeWithKeycloak } from './types';
+import Keycloak from 'keycloak-js';
+import { KeycloakAuthConfig } from '../service/KeycloakAuthConfig';
+import { UnauthorizedError } from '../errors/UnauthorizedError';
 
-export const authorizeWithKeycloak: AuthorizeWithKeycloak = () => {
+type AuthState = 'authorizing' | 'authorized';
+
+type AuthContext = {
+	readonly state: AuthState;
+	readonly config: KeycloakAuthConfig;
+	readonly keycloak: Keycloak;
+};
+
+const handleAuthorizing = (context: AuthContext): Promise<AuthContext> =>
+	context.keycloak.init({ onLoad: 'login-required' }).then((isSuccess) => {
+		if (isSuccess) {
+			return Promise.resolve({
+				...context,
+				state: 'role-check'
+			});
+		}
+		return Promise.reject(new UnauthorizedError());
+	});
+
+const handleAuthStep = (context: AuthContext): Promise<AuthContext> => {
+	switch (context.state) {
+		case 'authorizing':
+			return handleAuthorizing(context).then(handleAuthStep);
+		case 'authorized':
+		default:
+			return Promise.resolve(context);
+	}
+};
+
+export const authorizeWithKeycloak: AuthorizeWithKeycloak = (config) => {
+	const keycloak = new Keycloak({
+		url: config.authServerUrl,
+		realm: config.realm,
+		clientId: config.clientId
+	});
+	handleAuthStep({
+		config,
+		keycloak,
+		state: 'authorizing'
+	});
 	throw new Error();
 };
