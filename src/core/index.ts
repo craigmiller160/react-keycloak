e181f197c40ada360afcf0f985d502ce9ef3a689
Craig Miller <craigmiller160@gmail.com>
2023-02-04 18:45:02 -0500

    Implemented first test

---
+++ src/core/index.ts
@@ -0,0 +1,7 @@
+import { AuthorizeWithKeycloak, KeycloakAuthConfig } from './types';
+
+export const createKeycloakAuthorization = (
+	config: KeycloakAuthConfig
+): AuthorizeWithKeycloak => {
+	throw new Error();
+};
--- test/core/authorization.test.ts
+++ test/core/authorization.test.ts
@@ -1,9 +1,16 @@
 import { beforeEach, describe, it, vi } from 'vitest';
-import { ACCESS_TOKEN_EXP } from '../testutils/data';
+import {
+	ACCESS_TOKEN_EXP,
+	AUTH_SERVER_URL,
+	CLIENT_ID,
+	REALM,
+	TOKEN,
+	TOKEN_PARSED
+} from '../testutils/data';
 import { AuthorizeWithKeycloak } from '../../src/core/types';
-import react from '@vitejs/plugin-react';
 import { KeycloakAuthError } from '../../src/errors/KeycloakAuthError';
 import { KeycloakTokenParsed } from 'keycloak-js';
+import { createKeycloakAuthorization } from '../../src/core';
 
 const advancePastRefresh = () =>
 	vi.advanceTimersByTime(ACCESS_TOKEN_EXP * 1000 + 10);
@@ -54,7 +61,18 @@ describe('authorization', () => {
 	});
 
 	it('handles a successful authorization', async () => {
-		throw new Error();
+		const authorize = createKeycloakAuthorization({
+			realm: REALM,
+			authServerUrl: AUTH_SERVER_URL,
+			clientId: CLIENT_ID
+		});
+		const results = await promisify(1)(authorize);
+		expect(results).toEqual([
+			{
+				token: TOKEN,
+				tokenParsed: TOKEN_PARSED
+			}
+		]);
 	});
 
 	it('handles a failed authorization', async () => {
