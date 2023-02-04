774eebacfae80d8ee1956f1cb405f49d33b60236
Craig Miller <craigmiller160@gmail.com>
2023-02-04 18:58:25 -0500

    Beginning to implement the handler logic

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,28 @@
 import { AuthorizeWithKeycloak, KeycloakAuthConfig } from './types';
+import Keycloak from 'keycloak-js';
 
 export const createKeycloakAuthorization = (
 	config: KeycloakAuthConfig
 ): AuthorizeWithKeycloak => {
-	throw new Error();
+	const keycloak = new Keycloak({
+		url: config.authServerUrl,
+		realm: config.realm,
+		clientId: config.clientId
+	});
+	return (onSuccess, onFailure) => {
+		keycloak.onAuthSuccess = () => {
+			// TODO check for roles
+			onSuccess(keycloak.token!, keycloak.tokenParsed!);
+			// TODO trigger the refresh
+		};
+
+		keycloak.onAuthRefreshSuccess = () => {
+			// TODO check for roles
+			onSuccess(keycloak.token!, keycloak.tokenParsed!);
+			// TODO trigger the next refresh
+		};
+
+		keycloak.onAuthError = onFailure;
+		keycloak.onAuthRefreshError = onFailure;
+	};
 };
