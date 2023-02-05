470e8f42db63ff5d0ec4c1502ead6a80a2372296
Craig Miller <craigmiller160@gmail.com>
2023-02-05 08:18:54 -0500

    Further improved index exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { KeycloakAuthContext } from './react/KeycloakAuthContext';
 export type { KeycloakAuth, KeycloakAuthStatus } from './react/types';
+export type { RequiredRoles } from './core/types';
 export { KeycloakAuthProvider } from './react/KeycloakAuthProvider';
