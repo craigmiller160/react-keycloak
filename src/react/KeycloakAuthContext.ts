cac0479900cd971d13eb1b06786e796af253e5fc
Craig Miller <craigmiller160@gmail.com>
2023-02-05 12:45:55 -0500

    Fixed type error

--- src/react/KeycloakAuthContext.ts
+++ src/react/KeycloakAuthContext.ts
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 import { KeycloakAuth } from './types';
 
 export const KeycloakAuthContext = createContext<KeycloakAuth>({
-	status: 'pre-auth',
+	status: 'authorizing',
 	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	logout: () => {}
 });
