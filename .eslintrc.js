791ab925ce8ac20a6ab54c4cf67533e7370fe7f9
Craig Miller <craigmiller160@gmail.com>
2023-07-25 09:04:02 -0400

    More refactoring to fix libs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
 		'@craigmiller160/eslint-config-js',
 		'@craigmiller160/eslint-config-prettier',
 		'@craigmiller160/eslint-config-react',
-		'@craigmiller160/eslint-config-tree-shaking-import-restrictions',
 		'@craigmiller160/eslint-config-ts'
 	]
 };
