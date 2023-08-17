12dbbda78bc768f30e753a5a35c95bd5b89879df
Craig Miller <craigmiller160@gmail.com>
2023-08-17 13:17:43 -0400

    Adding back in core stuff

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { KeycloakAuthContext } from './react/KeycloakAuthContext';
 export type { KeycloakAuth, KeycloakAuthStatus } from './react/types';
-export type { RequiredRoles } from './core/types';
+export type { RequiredRoles } from '@craigmiller160/keycloak-js';
 export { KeycloakAuthProvider } from './react/KeycloakAuthProvider';
